81415d96e301494433dc3195d643f756360968a7
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2023-05-16 06:45:20 +0530

    feat: uploaded backend

--- backend/ingestion.py
+++ backend/ingestion.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI, status, Request
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client, Client
-import urllib.request 
+from pigwrite import XMLParser
+import subprocess
+import os 
+import uuid 
+import shutil
 
 url = "https://pkwxbrxicmbncxgcblbv.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrd3hicnhpY21ibmN4Z2NibGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQxNjEzMDksImV4cCI6MTk5OTczNzMwOX0.AlBih_pbLn3jhcPqp0o-a0CP-80nO02PD7lM4UtiVuA";
@@ -31,6 +35,7 @@ class Ingestion(BaseModel):
 
 @app.get("/")
 async def root():
+    os.system("pwd")
     return {"message": "hello world"}
 
 
@@ -50,6 +55,19 @@ async def ingest_files(ingestion: Ingestion):
     with open("./datafile.tsv", 'wb+') as f:
         res = supabase.storage.from_("pig_storage").download(ingestion.datafilePath)
         f.write(res)
-            
+        
+    
+    xmlParser = XMLParser("./pig_ingest.xml")
+    xmlParser.parseXML()
+    xmlParser.writeScript()
+    text_as_string = open('./yagowordcounts/part-r-00000', 'r').read()
 
-    return {"ingestion": "ok"}
+    link_to_pig = ""
+    unique_file = str(uuid.uuid4()) + ".pig"
+
+    with open("./pig_script.pig", 'rb+') as f:
+        res = supabase.storage.from_("pig_storage").upload(unique_file, os.path.abspath("./pig_script.pig"))
+
+    piglink = supabase.storage.from_("pig_storage").get_public_url(unique_file)
+
+    return {"ingestion": text_as_string, "piglink": piglink}
---
+++ backend/pigwrite.py
@@ -0,0 +1,159 @@
+# importing the required packages
+import os
+import json
+import xml.etree.ElementTree as ET
+import shutil 
+
+
+class XMLParser:
+    def __init__(self, xmlFile):
+        """
+        object takes in the required XML file and creates an XML tree for parsing
+        """
+        self.xmlFile = xmlFile
+        self.tree = ET.parse(self.xmlFile)
+        self.root = self.tree.getroot()
+
+        self.processes = []
+        self.pigFile = open("pig_script.pig","w+")
+        self.dataset = None
+        self.typeDef = {}
+        self.dumpVariable = "records"
+        self.outputFile = "./pig_xml"
+        self.defaultName = "records"
+
+
+
+
+    def parseXML(self):
+        """
+        parse XML method which given an XML file, parses it and gets all the processes
+        """
+
+        for item in self.root.findall(".//"):
+            # iterate child elements of item
+
+            if item.tag == "inputfile":
+                self.dataset = item.text
+                self.dataset = "./datafile.tsv"
+
+
+            elif item.tag == "defaultname":
+                self.defaultName = item.text
+
+            elif item.tag == "delimiter":
+                self.delimiter = item.text
+
+
+            elif item.tag == "dumpvar":
+                self.dumpVariable = item.text
+
+
+            elif item.tag == "outputfile":
+                self.outputFile = item.text
+
+            elif item.tag == "columns":
+                col = None
+                for typedef in item:
+                    if typedef.tag == "col":
+                        col = typedef.text
+                    elif typedef.tag == "type":
+                        self.typeDef[col] = typedef.text
+                        col = None
+
+
+            # checks for the process tag and iterates over the child elements of process
+            elif item.tag == "process":
+                for child in item:
+                    if child.tag == "name":
+                        process = {}
+                        process["name"] = child.text
+                        self.processes.append(process)
+
+                    elif child.tag == "table":
+                        self.processes[-1]["table"] = child.text
+
+                    elif child.tag == "table1":
+                        self.processes[-1]["table1"] = child.text
+
+                    elif child.tag == "column":
+                        self.processes[-1]["column"] = child.text
+
+                    elif child.tag == "column1":
+                        self.processes[-1]["column1"] = child.text
+
+                    elif child.tag == "variable":
+                        self.processes[-1]["variable"] = child.text
+
+                    elif child.tag == "variable1":
+                        self.processes[-1]["variable1"] = child.text
+
+                    elif child.tag == "task":
+                        self.processes[-1]["task"] = child.text
+
+                    elif child.tag == "condition":
+                        self.processes[-1]["condition"] = child.text
+
+                    elif child.tag == "clause":
+                        self.processes[-1]["clause"] = child.text
+
+
+
+    def writeScript(self):
+        typeString = ""
+        for key, value in self.typeDef.items():
+            typeString += "{}:{},".format(key,value)
+        typeString = typeString[:-1]
+
+        if self.delimiter == "space":
+            loadLine = "records = LOAD '{}' USING PigStorage(' ') AS ({});\n".format(self.dataset, typeString)
+            self.pigFile.write(loadLine)
+
+
+        previousVariable = self.defaultName
+        for process in self.processes:
+            if process["task"] == "filter":
+                scriptline = "{} = FILTER {} by {} {} '{}';\n".format(process["name"], previousVariable, process["column"], process["condition"], process["clause"])
+                self.pigFile.write(scriptline)
+
+            elif process["task"] == "group":
+                scriptline = "{} = GROUP {} by {};\n".format(process["name"], previousVariable, process["column"])
+                self.pigFile.write(scriptline)
+
+            elif process["task"] == "distinct":
+                scriptline = "{} = DISTINCT {};\n".format(process["name"], process["table"])
+                self.pigFile.write(scriptline)
+
+            elif process["task"] == "cross":
+                scriptline = "{} = CROSS {},{};\n".format(process["name"], process["table"], process["table1"])
+                self.pigFile.write(scriptline)
+
+            elif process["task"] == "join":
+                scriptline = "{} = JOIN {} BY {},{} BY {};\n".format(process["name"], process["table"], process["column"], process["table1"], process["column1"])
+                self.pigFile.write(scriptline)
+
+            elif process["task"] == "limit":
+                scriptline = "{} =  LIMIT {} {};\n".format(process["name"], process["table"], process["clause"])
+                self.pigFile.write(scriptline)
+
+            elif process["task"] == "order":
+                scriptline = "{} =  ORDER {} BY {};\n".format(process["name"], process["table"], process["column"])
+                self.pigFile.write(scriptline)
+
+            elif process["task"] == "foreach":
+                scriptline = "{} =  FOREACH {} GENERATE {} AS {},COUNT({}) AS {};\n".format(process["name"], process["table"], process["column"], process["variable"], process["column1"], process["variable1"])
+                self.pigFile.write(scriptline)
+
+
+
+        storeLine = "STORE {} INTO '{}';".format(self.dumpVariable, self.outputFile)
+        self.pigFile.write(storeLine)
+        self.pigFile.close()
+
+        self.executeScript()
+
+
+    def executeScript(self):
+        os.system("pwd")
+        shutil.rmtree("./yagowordcounts")
+        os.system("pig -x local pig_script.pig")
